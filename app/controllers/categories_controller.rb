91d2a761c35b93b6ecd03bf896ff95c9ce4bf2b5
Yura Voevodin <voevodin.yura@icloud.com>
2022-06-26 19:08:55 +0300

    Improvments

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,10 +21,10 @@ class CategoriesController < ApplicationController
       { name: "10_03_2021", first_photo: "15.jpg" },
       { name: "07_11_2021", first_photo: "1.jpg" },
       { name: "16_05_2021", first_photo: "1.jpg" },
-      { name: "26_02_2021", first_photo: "1.jpg" },
-      { name: "31_10_2021", first_photo: "2.jpg" },
-      { name: "23_01_2021", first_photo: "2.jpg" },
-      { name: "11_08_2021", first_photo: "3.jpg" },
+      { name: "26_02_2021", first_photo: "3.jpg" },
+      { name: "31_10_2021", first_photo: "15.jpg" },
+      { name: "23_01_2021", first_photo: "12.jpg" },
+      { name: "11_08_2021", first_photo: "16.jpg" },
       { name: "11_07_2021", first_photo: "14.jpg" }
     ]
     @title = "Індивідуальні фотосесіЇ"
