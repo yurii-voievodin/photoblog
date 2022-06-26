4b5b14f7dfac6991856a9f2c30d255afe4033e4a
Yura Voevodin <voevodin.yura@icloud.com>
2022-06-26 15:11:15 +0300

    Add page titles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,16 @@
 class ArticlesController < ApplicationController
   def index
+    @title = "Марина Селегень - портфоліо"
   end
 
   def prices
+    @title = "Марина Селегень - ціни на зйомку"
   end
 
   def reviews
   end
 
   def contacts
+    @title = "Марина Селегень - контакти"
   end
 end
